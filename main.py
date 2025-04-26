c662c96a629a6912ed5e5e8f8fdba3799270c003
Sakan Nirattisaykul <sakan.nir@gmail.com>
2025-04-27 02:21:27 +0700

    Update .gitignore and refactor main.py for improved logging and structure

--- main.py
+++ main.py
@@ -9,28 +9,33 @@ from simulations.visuals.plot_eidolon_value import (
     calculate_marginal_value,
     plot_eidolon_value,
 )
+import logging
+
+# Set up default logger
+logger = logging.getLogger("hsr_eidolon_value_analysis")
+logger.setLevel(logging.INFO)
+if not logger.hasHandlers():
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(module)s:%(lineno)d -  %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
 
 def main():
-    # Initialize character
+    # Initialize characters
     character_list: list[Character] = [RuanMei(), Castorice()]
 
-    # Run simulations
     for character in character_list:
-        print(f"Running simulations for {character.get_name()}...")
+        logger.info(f"Running simulations for {character.get_name()}...")
         simulation_data = run_simulations(character)
 
-        print("\nGenerating plots...")
+        logger.info("Generating plots...")
 
-        # Convert simulation data to normalized damage percentages
         avg_dmg = convert_simulation_data_to_avg_dmg(simulation_data)
-
-        # Calculate the pull efficiency metrics
         pulls_per_eidolon = calculate_pulls_per_eidolon()
         dmg_per_pull = calculate_dmg_per_pull(avg_dmg, pulls_per_eidolon)
         marginal_value = calculate_marginal_value(avg_dmg, pulls_per_eidolon)
 
-        # Generate plots
         plot_eidolon_value(
             avg_dmg, dmg_per_pull, marginal_value, character_name=character.get_name()
         )
