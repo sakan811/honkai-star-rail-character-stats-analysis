19bec60c84fd737b5d5644a8831cb86e40723869
Sakan Nirattisaykul <sakan.nir@gmail.com>
2025-05-04 16:20:08 +0700

    feat: add site title and description constants for the application

---
+++ visual_dashboard/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE =
+  "Honkai: Star Rail Eidolon and Signature Lightcone Value Analysis";
+export const SITE_DESCRIPTION =
+  "Analyze the value proposition of different Eidolon levels and signature lightcone for characters in Honkai: Star Rail";
