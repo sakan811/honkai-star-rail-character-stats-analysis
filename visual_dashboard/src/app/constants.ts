ffc50b68580732dc807db618f762a0034b28a8d4
Sakan Nirattisaykul <sakan.nir@gmail.com>
2025-05-21 00:12:39 +0700

    refactor: update README and constants for consistency in character stats analysis

--- visual_dashboard/src/app/constants.ts
+++ visual_dashboard/src/app/constants.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Honkai: Star Rail Character Analysis";
-export const SITE_DESCRIPTION = "Analyze Honkai: Star Rail's characters stats based on different scenarios.";
+export const SITE_TITLE = "Honkai: Star Rail Character Stats Analysis";
+export const SITE_DESCRIPTION = "Analyze Honkai: Star Rail's characters stats based on different scenarios or their abilities.";
