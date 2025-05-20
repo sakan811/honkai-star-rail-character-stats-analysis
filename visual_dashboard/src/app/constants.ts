b71b3dacc7f13efd0142c7cb851255ab79660fde
Sakan Nirattisaykul <sakan.nir@gmail.com>
2025-05-21 00:08:46 +0700

    feat: add Anaxa and Castorice pages with data visualization; update constants and remove obsolete dashboard

--- visual_dashboard/src/app/constants.ts
+++ visual_dashboard/src/app/constants.ts
@@ -1,4 +1,2 @@
-export const SITE_TITLE =
-  "Honkai: Star Rail Eidolon and Signature Lightcone Value Analysis";
-export const SITE_DESCRIPTION =
-  "Analyze the value proposition of different Eidolon levels and signature lightcone for characters in Honkai: Star Rail";
+export const SITE_TITLE = "Honkai: Star Rail Character Analysis";
+export const SITE_DESCRIPTION = "Analyze Honkai: Star Rail's characters stats based on different scenarios.";
