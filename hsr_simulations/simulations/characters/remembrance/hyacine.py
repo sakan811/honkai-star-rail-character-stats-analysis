187cc0b96e254aed3fc0b18410d609fbc4651c31
Sakan Nirattisaykul <sakan.nir@gmail.com>
2025-05-21 22:58:09 +0700

    delete: remove hyacine test file and associated mocks

--- hsr_simulations/simulations/characters/remembrance/hyacine.py
+++ hsr_simulations/simulations/characters/remembrance/hyacine.py
@@ -1,10 +1,13 @@
-from ctypes import Union
 from simulations.characters.base_character import Character
 
 
 class Hyacine(Character):
     CONDITIONED_SPEED = 200
     MAX_SPEED = 400
+    MINOR_TRACES_SPEED = 14
+    SIGNATURE_LIGHTCONE_SPEED_MULT = 0.18
+    RELICS_SPEED_MULT = 0.12
+    PLANETARY_SPEED_MULT = 0.06
 
     def __init__(self) -> None:
         self.speed = 110
@@ -14,7 +17,14 @@ class Hyacine(Character):
             "character": [],
             "speed": [],
             "increased_outgoing_healing": [],
+            "base_speed": [],
+            "speed_after_minor_traces": [],
+            "speed_after_signature_lightcone": [],
+            "speed_after_relics_and_planetary_sets": [], 
         }
+        
+        speed_after_minor_traces = self.speed + self.MINOR_TRACES_SPEED
+        
         for speed in range(self.speed, self.MAX_SPEED + 1):
             if speed > self.CONDITIONED_SPEED:
                 exceed_speed = speed - self.CONDITIONED_SPEED
@@ -26,6 +36,12 @@ class Hyacine(Character):
                 data_dict["character"].append(self.__class__.__name__)
                 data_dict["speed"].append(speed)
                 data_dict["increased_outgoing_healing"].append(0.0)
+                
+            data_dict["base_speed"].append(self.speed)
+            data_dict["speed_after_minor_traces"].append(speed_after_minor_traces)
+            data_dict["speed_after_signature_lightcone"].append(speed_after_minor_traces * (1 + self.SIGNATURE_LIGHTCONE_SPEED_MULT))
+            data_dict["speed_after_relics_and_planetary_sets"].append(speed_after_minor_traces * (1 + self.RELICS_SPEED_MULT) * (1 + self.PLANETARY_SPEED_MULT))
+                
         return data_dict
         
     def output_data(self) -> dict[str, list[str | float]]:
