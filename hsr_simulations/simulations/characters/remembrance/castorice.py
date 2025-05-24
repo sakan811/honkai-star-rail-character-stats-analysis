3eac3bbb91bb0a47d4c5e386f64e4b78d3eaf83a
Sakan Nirattisaykul <sakan.nir@gmail.com>
2025-05-24 23:06:52 +0700

    feat: Add heal count tracking to Castorice calculations and update CSV data

--- hsr_simulations/simulations/characters/remembrance/castorice.py
+++ hsr_simulations/simulations/characters/remembrance/castorice.py
@@ -5,6 +5,7 @@ class Castorice(Character):
     NEWBUD = 34000
     MIN_COMBINED_ALLIES_HP = 8000
     MAX_COMBINED_ALLIES_HP = 32000
+    GALLAGHER_SKILL_HEAL = 1600
 
     def __init__(self):
         self.current_combined_allies_hp = 0
@@ -14,6 +15,7 @@ class Castorice(Character):
             "character": [],
             "combined_allies_hp": [],
             "skill_count_before_getting_ult": [],
+            "heal_count_before_getting_ult": [],
         }
                 
         for combined_allies_hp in range(
@@ -21,20 +23,24 @@ class Castorice(Character):
         ):        
             current_newbud = 0
             self.current_combined_allies_hp = combined_allies_hp
-            counter = 0
+            skill_counter = 0
+            heal_counter = 0
             while current_newbud < self.NEWBUD:
-                current_newbud += self.current_combined_allies_hp * 0.3
-                counter += 1
-                self.current_combined_allies_hp *= 0.7
-                
-                heal = self.current_combined_allies_hp * 0.5
-                self.current_combined_allies_hp += heal
-                
-                current_newbud += heal
+                allies_hp_consumed = self.current_combined_allies_hp * 0.3
+                current_newbud += allies_hp_consumed
+                skill_counter += 1
+                if current_newbud >= self.NEWBUD:
+                    break
+                self.current_combined_allies_hp -= allies_hp_consumed
+                current_newbud += self.GALLAGHER_SKILL_HEAL
+                heal_counter += 1
+                if current_newbud >= self.NEWBUD:
+                    break
                 
             data_dict["character"].append(self.__class__.__name__)
             data_dict["combined_allies_hp"].append(combined_allies_hp)
-            data_dict["skill_count_before_getting_ult"].append(counter)
+            data_dict["skill_count_before_getting_ult"].append(skill_counter)
+            data_dict["heal_count_before_getting_ult"].append(heal_counter)
 
         return data_dict
     
