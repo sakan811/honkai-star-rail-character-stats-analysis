7a26cc3b1a90c063da6e4ce407c116866f3a66ae
Sakan Nirattisaykul <sakanbeer@gmail.com>
2025-05-28 13:50:49 +0700

    feat: Add chart test utility functions for rendering and mocking data

---
+++ visual_dashboard/__tests__/utils/chart-test-utils.tsx
@@ -0,0 +1,22 @@
+import { render, waitFor } from "@testing-library/react";
+import { expect, vi } from "vitest";
+
+export const setupChartTest = async (Component: React.ComponentType) => {
+  render(<Component />);
+
+  await waitFor(() => {
+    expect(screen.queryByTestId("loading-spinner")).not.toBeTruthy();
+  });
+};
+
+export const mockChartData = (data: any[]) => {
+  const Papa = vi.mocked(await import("papaparse"));
+  Papa.default.parse = vi.fn((_, options) => {
+    if (options.complete) {
+      options.complete({
+        data,
+        meta: { fields: Object.keys(data[0]) },
+      });
+    }
+  });
+};
