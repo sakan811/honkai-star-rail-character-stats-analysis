c0b439a69d655fb0bc653002abe0f805444895d5
Sakan Nirattisaykul <sakan.nir@gmail.com>
2025-06-03 00:29:10 +0700

    fix: Standardize string quotes and formatting in manifest, robot, and sitemap files

--- visual_dashboard/src/app/robot.ts
+++ visual_dashboard/src/app/robot.ts
@@ -1,14 +1,15 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://honkai-star-rail-character-stats-analysis.vercel.app'
-  
+  const baseUrl =
+    "https://honkai-star-rail-character-stats-analysis.vercel.app";
+
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/private/'],
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/api/", "/private/"],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
-  }
+  };
 }
